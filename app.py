b613eb3bddf83709a0a20a25daabd272d65a1518
cbnaattoh <chrisbrandtattoh4@gmail,com>
2025-06-07 23:31:08 +0000

    Trigger redeploy

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+print("Triggering deployment")
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5000)
+
